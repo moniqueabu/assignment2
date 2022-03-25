13abb8c8258e978a6d16675d128ba8ba12f2a4cd
Monique Aboumrad <mas1790@nyu.edu>
2022-03-25 15:07:13 -0400

    Moniques initial code

---
+++ My project (2)/Assets/PlayerCode.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerCode : MonoBehaviour
+{
+
+    NavMeshAgent _navAgent;
+    Camera mainCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navAgent = GetComponent<NavMeshAgent>();
+        mainCam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out hit, 200))
+            {
+                _navAgent.destination = hit.point;
+
+            }
+        }
+    }
+
+
+
+}
